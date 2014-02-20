d14367a1d148e8326ff52ba8606303b94433cbc8
arsood <arsood@gmail.com>
2014-02-20 14:46:24 -0800

    Fix

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Backbone
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :put]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
       end
     end
   end
