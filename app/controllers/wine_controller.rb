655dc2448451c83eeb50ac8d8d9644440c6adfa7
arsood <arsood@gmail.com>
2014-02-21 16:23:45 -0800

    Fix

--- app/controllers/wine_controller.rb
+++ app/controllers/wine_controller.rb
@@ -10,7 +10,22 @@ class WineController < ApplicationController
 	end
 
 	def create
-		render :text => 'Success! You created a new record'
+		new_wine = Wine.new
+
+		new_wine.name = params[:name]
+		new_wine.year = params[:year]
+		new_wine.grapes = params[:grapes]
+		new_wine.country = params[:country]
+		new_wine.region = params[:region]
+		new_wine.description = params[:description]
+		new_wine.picture = params[:picture]
+		
+		if new_wine.save
+			render :json => '{"result":"Success! You created a new record"}'
+		else
+			render :json => '{"result":"There was an error..."}'
+		end
+
 	end
 
 	def update
